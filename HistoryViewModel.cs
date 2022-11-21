a57e140b2a043a6bcc3edfc0a8caf8973b38cf71
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-11-20 19:45:48 -0600

    Update HistoryViewModel.cs

--- HistoryViewModel.cs
+++ HistoryViewModel.cs
@@ -9,10 +9,32 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-public class HistoryViewModel:INotifyPropertyChanged
+  public class HistoryViewModel:INotifyPropertyChanged
+  {
+   public class HistoryViewModel:INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<String> ExpressionHistory;
+
+        public HistoryViewModel()
+        {
+            ExpressionHistory = new ObservableCollection<String>();
+        }
+
+        public void Add(String calculation)
+        {
+            ExpressionHistory.Add(calculation);
+            OnPropertyChanged("historyExpressions");
+            OnPropertyChanged();
+        }
+
+        public ObservableCollection<String> historyExpressions
+        {
+            get => ExpressionHistory;
+        }
        
         public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
-    }
+  }
 }
