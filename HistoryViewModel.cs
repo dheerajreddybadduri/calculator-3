a0ad2c1f366cb96a738c5f66756b02e0878d4977
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-11-20 19:36:45 -0600

    Update HistoryViewModel.cs

--- HistoryViewModel.cs
+++ HistoryViewModel.cs
@@ -15,17 +15,6 @@ namespace Calculator
 
         private ObservableCollection<String> ExpressionHistory;
 
-        public HistoryViewModel()
-        {
-            ExpressionHistory = new ObservableCollection<String>();
-        }
-
-        public void Add(String calculation)
-        {
-            ExpressionHistory.Add(calculation);
-            OnPropertyChanged("historyExpressions");
-            OnPropertyChanged();
-        }
 
         public ObservableCollection<String> historyExpressions
         {
