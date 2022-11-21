10b748b9dbe67dab20d376748224d459e001081f
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-11-20 19:29:08 -0600

    Add files via upload

--- HistoryViewModel.cs
+++ HistoryViewModel.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -6,3 +6,33 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class HistoryViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<String> ExpressionHistory;
+
+        public HistoryViewModel()
+        {
+            ExpressionHistory = new ObservableCollection<String>();
+        }
+
+        public void Add(String calculation)
+        {
+            ExpressionHistory.Add(calculation);
+            OnPropertyChanged("historyExpressions");
+            OnPropertyChanged();
+        }
+
+        public ObservableCollection<String> historyExpressions
+        {
+            get => ExpressionHistory;
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    }
+}
